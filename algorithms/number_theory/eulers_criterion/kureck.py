9129d2f6e69a33417711eb085e4fdbb5adc51ddd
ekureck <eric.kureck@walmart.com>
2015-06-08 10:51:57 -0300

    adds kurecks eulers criterion resolution

---
+++ algorithms/number_theory/eulers_criterion/kureck.py
@@ -0,0 +1,13 @@
+def kureck_eulers_criterion(a, p):
+    # a**((p-1)/2) = 1 % p
+    # return "YES" if a**((p-1)/2) % p == 1 else "NO"
+    return "YES" if pow(a, (p-1)/2, p) in (0, 1) else "NO"
+
+n = int(raw_input())
+data = []
+for n in xrange(0, n):
+    values = raw_input()
+    data.append([int(x) for x in values.split(" ")])
+
+for d in data:
+    print kureck_eulers_criterion(d[0], d[1])
