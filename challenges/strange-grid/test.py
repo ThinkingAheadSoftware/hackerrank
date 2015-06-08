91f32845d5dc3b71ce2d79a99aba4790aeeedc5c
Fernando Luz <presidente.ifsc@gmail.com>
2015-06-08 16:48:06 -0300

    inclusion of test case and instructions for strange-grid

---
+++ challenges/strange-grid/test.py
@@ -0,0 +1,22 @@
+from fernando import strange_grid
+# from kureck import strange_grid
+
+print strange_grid(6, 3)
+
+assert(strange_grid(6, 3) == 25)
+
+print strange_grid(1, 1)
+
+assert(strange_grid(1, 1) == 0)
+
+print strange_grid(500, 5)
+
+assert(strange_grid(500, 5) == 2498)
+
+print strange_grid(500, 5)
+
+assert(strange_grid(500, 5) == 2498)
+
+print strange_grid(2000000000, 3)
+
+assert(strange_grid(2000000000, 3) == 4999999996)
