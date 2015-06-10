d360677cd5c24a06e46241f42f31f5766161c108
ekureck <eric.kureck@walmart.com>
2015-06-10 10:27:21 -0300

    removes create infinity natrix method

--- challenges/strange-grid/kureck.py
+++ challenges/strange-grid/kureck.py
@@ -2,20 +2,8 @@ def strange_grid():
     pass
 
 
-def create_infinity_matrix():
-    max_col = 5
-    max_row = 2*(10**9)
-    M = []
-    for i in xrange(0, max_row - 1):
-        r = []
-        for j in xrange(0, max_col - 1):
-            r.append(j)
-        M.append(r)
-    return M
-
-
 def run():
-    print create_infinity_matrix()
+    pass
 
 if __name__ == '__main__':
     run()
