dccadb9f08e9294171fd6f0c0fef4854eefcc389
Fernando Luz <presidente.ifsc@gmail.com>
2015-06-10 10:52:56 -0300

    acerto dos casos de teste

--- challenges/strange-grid/fernando_test.py
+++ challenges/strange-grid/fernando_test.py
@@ -10,12 +10,12 @@ assert(strange_grid(1, 1) == 0)
 
 print strange_grid(500, 5)
 
-assert(strange_grid(500, 5) == 2498)
+assert(strange_grid(500, 5) == 2499)
 
-print strange_grid(500, 5)
+print strange_grid(501, 5)
 
-assert(strange_grid(500, 5) == 2498)
+assert(strange_grid(501, 5) == 2508)
 
-print strange_grid(2000000000, 3)
+print strange_grid(2000000001, 3)
 
-assert(strange_grid(2000000000, 3) == 4999999996)
+assert(strange_grid(2000000001, 3) == 10000000004)
