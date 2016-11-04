a26b462f17a6e318ad8a509d0f6be2be8ac1c6ac
Eric Kureck <ekureck@bionexo.com>
2016-11-04 19:21:51 -0200

    solves stack balanced bracket

--- cracking-the-code-interview/data-structures/stack-balanced-brackets.py
+++ cracking-the-code-interview/data-structures/stack-balanced-brackets.py
@@ -1,28 +1,5 @@
 # https://www.hackerrank.com/challenges/ctci-balanced-brackets
 
-# list = ['{', '[', '(', ')', ']', '}']
-# stack = []
-# { take element from list
-# look if stack is empty, if it is, push element
-# list = [[', '(', ')', ']', '}']
-# stack = [{]
-# [ next element, check if it is its counterpart
-# stack = [{,[]
-# ( next element. check ..
-# stack = [{,[,(]
-# ) next element, check. It is. Pop element from stack
-# stack = [{,[]
-# ] next, check, it is. Pop element
-# stack = [{]
-# } next, check, it is. pop element
-# stack is empty? and stack is empty? True, else False
-#
-#
-# take the first element (opening bracket) and verify if its counterpart is the first in the stack
-# while is opening brackets do the push operation
-# if the element taken from list matches its counterpart on the stack, pop element from stack
-# else if the element is a closing bracket but it's not from the same type return error
-# else
 def is_matched(expression):
     counterparts = {
         '{' : '}', '}' : '{',
@@ -36,6 +13,9 @@ def is_matched(expression):
     if expression:
         stack.append(expression.pop(0))
         for e in expression:
+            if not stack:
+                stack.append(e)
+                continue
             if counterparts[e] == stack[-1]:
                 stack.pop()
             else:
